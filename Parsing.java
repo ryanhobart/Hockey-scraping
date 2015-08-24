e954f1ba1f6ebaec11661393c132ad7daf4710d7
ryanhobart <ryan.david.hobart@gmail.com>
2015-08-24 13:16:26 -0400

    Add Scraping.java

--- Parsing.java
+++ Parsing.java
@@ -1,9 +1,79 @@
+/* The purpose of this class is
+*
+*/
 package hockeyscraping;
+
+import java.util.List;
 
 /**
  *
  * @author Ryan
  */
 public class Parsing {
+    public static List<String[]> tableDataTokens;
+    
+    public Parsing(List<String[]> tableDataTokens){
+        this.tableDataTokens = tableDataTokens;        
+    }
+    
+    public static String manStrength(int index){
+        return tableDataTokens.get(index)[2];
+    }
+    public static String eventType(int index){
+        return tableDataTokens.get(index)[5];
+    }
+    public static String[] playersOnIce(int index){
+        String[] players = new String[10]; //array of player numbers
+        if(Parsing.eventType(index).equals("BLOCK")){
+            //tokens 17, 20, 23, are away FWDs, 
+            // 26, 29, are away D
+            // 34, 37 and 40 are home FWDs
+            // 43, 46 are home D
+            players[0] = tableDataTokens.get(index)[17];
+            players[1] = tableDataTokens.get(index)[20];
+            players[2] = tableDataTokens.get(index)[23];
+            players[3] = tableDataTokens.get(index)[26];
+            players[4] = tableDataTokens.get(index)[29];
+            players[5] = tableDataTokens.get(index)[34];
+            players[6] = tableDataTokens.get(index)[37];
+            players[7] = tableDataTokens.get(index)[40];
+            players[8] = tableDataTokens.get(index)[43];
+            players[9] = tableDataTokens.get(index)[46];
+            
+        }
+        if(Parsing.eventType(index).equals("MISS")){
+            //tokens 15, 18, 21 are away FWDs, 
+            // 24, 27 are away D
+            // 32, 35, 38 are home FWDs
+            // 41, 44 are home D
+            players[0] = tableDataTokens.get(index)[15];
+            players[1] = tableDataTokens.get(index)[18];
+            players[2] = tableDataTokens.get(index)[21];
+            players[3] = tableDataTokens.get(index)[24];
+            players[4] = tableDataTokens.get(index)[27];
+            players[5] = tableDataTokens.get(index)[32];
+            players[6] = tableDataTokens.get(index)[35];
+            players[7] = tableDataTokens.get(index)[48];
+            players[8] = tableDataTokens.get(index)[41];
+            players[9] = tableDataTokens.get(index)[44];
 
+        }
+        if(Parsing.eventType(index).equals("SHOT")){
+            //tokens 16, 19, 22, are away FWDs, 
+            // 25, 28, are away D
+            // 33, 36 and 39 are home FWDs
+            // 42, 45 are home D
+            players[0] = tableDataTokens.get(index)[16];
+            players[1] = tableDataTokens.get(index)[19];
+            players[2] = tableDataTokens.get(index)[22];
+            players[3] = tableDataTokens.get(index)[25];
+            players[4] = tableDataTokens.get(index)[28];
+            players[5] = tableDataTokens.get(index)[33];
+            players[6] = tableDataTokens.get(index)[36];
+            players[7] = tableDataTokens.get(index)[39];
+            players[8] = tableDataTokens.get(index)[42];
+            players[9] = tableDataTokens.get(index)[45];
+        }
+        return players;    
+    }
 }
--- Scraping.java
+++ Scraping.java
@@ -1,3 +1,8 @@
+/* The purpose of this class is to access the supplied webpage to determine if it exists, and if it does,
+* to grab all the table data from it and provide it back to the main class as an ArrayList of ArrayLists of Strings. Each string
+represents a token in the table, so the 2d ArrayList is a direct representation of the table.
+*/
+
 package hockeyscraping;
 
 import java.util.Collections;
@@ -7,6 +12,7 @@ import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  *
@@ -14,16 +20,16 @@ import org.jsoup.nodes.Element;
  */
 public class Scraping {
     
-    static String link = null;
     private static List<String[]> ListLineTokens = null;
+    String tableClass;
     
     //instantiation for scraping adds the link and creats the 2d array list of strings
-    public static void start(String link){
-        Scraping.link = link;
+    public Scraping(){
         ListLineTokens = new ArrayList<String[]>(); //ArrayList implementation for real use
     }
+    
 
-    public List<String[]> getLineTokens() {
+    public List<String[]> getGameData(String link) {
 
         Document doc = null;
         String line = null;
@@ -31,7 +37,6 @@ public class Scraping {
         //ArrayList<String> temp = new ArrayList<String>(); 
         String[] tempLineTokens = null;
         //String[][] arrayLineTokens = new String[400][60]; //Array implementation for quick testing
-        int i = 0;
         try{ //attempt to connect to the link provided
             doc = Jsoup.connect(link).maxBodySize(0).get();
         
@@ -54,4 +59,66 @@ public class Scraping {
 
         return ListLineTokens;
     }
-}
+    
+    public List<String[]> getPlayerNames(String link){
+        Document doc = null;
+        String line = null;
+        String delims = "[ ]+"; //within the square braces is the delimiter for the elements. 
+        //ArrayList<String> temp = new ArrayList<String>(); 
+        String[] tempLineTokens = null;
+        
+        List<String[]> rosterNames = new ArrayList<String[]>();
+        //String[][] arrayLineTokens = new String[400][60]; //Array implementation for quick testing
+        try{ //attempt to connect to the link provided
+            doc = Jsoup.connect(link).maxBodySize(0).get();
+        
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        //these next lines traverse the tree in the HTML code to find the table we're looking for
+        Element page = doc.getElementById("page_container");
+        Element content = doc.getElementById("page_content");
+        Element all_roster = content.getElementById("all_roster");
+        Element roster = all_roster.getElementById("roster");
+        
+        for(Element table : roster.select("table")){
+            for(Element row : table.select("table")){
+                line = row.text();//get the text from the row
+                tempLineTokens = line.split(delims);//split it into tokens
+            }
+        }
+        /*The data for the roster comes in one long line of string tokens
+        * The following for loop organizes the string tokens into an array list for
+        * the whole roster which will more closely represent the table fromt the website.
+        * This will be easier to search and utilize.
+        */
+        int goalieBuffer = 0; /*this buffer prevents the tempLineTokens counter from getting ahead of the
+        * temp counter when there's a goalie to add to the roster. Goalie lines are 3 tokens shorter,
+        * so for each goalie, the internal for loop needs to run 3 times without adding data from tempLineTokens.
+        * After it does this, goalieBuffer increases by 3, and is subtracted from the index when accessing data in
+        * tempLineTokens to prevent getting ahead.
+        */
+        for(int j = 11; j < tempLineTokens.length; j=j+18){
+
+            String [] temp = new String[18];            
+            for(int i = 0; i < 18; i++){
+                if(j+14 < tempLineTokens.length){ //this checks to make sure the look ahead for GAA doesn't exceed the limits of tempLineTokens
+                    if(j+i < tempLineTokens.length && !tempLineTokens[j+14-goalieBuffer].equals("GAA")){ //this check if it's a goalie line, and makes sure we're staying inside the limits of tempLineTokens
+                        //code for adding a skater line
+                        temp[i] = tempLineTokens[j+i-goalieBuffer];
+                    }else{
+                        //code for adding a goalie line
+                        if(i < 15)
+                            temp[i] = tempLineTokens[j+i-goalieBuffer];
+                        else if(15 <= i && i <18)
+                          temp[i] = "";
+                 }}}
+            //code for increasing goalieBuffer. Has to be outside of adding the goalie line, so that it is only increased once per goalie line
+            if(j+14 < tempLineTokens.length){
+                if(tempLineTokens[j+14-goalieBuffer].equals("GAA")){
+                        goalieBuffer = goalieBuffer + 3;
+            }}
+            rosterNames.add(temp);//adds the created string array to the roster list
+        }
+        return rosterNames;
+    }}
